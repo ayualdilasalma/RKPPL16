5c6ade4ffeac8ca0ec83951edaddaf266564350d
dilaaa <ayu.aldilas@yahoo.com>
2016-03-04 13:59:08 +0700

    Commit Github

--- Latihan-Pertemuan1/src/VersionalControl/KelasLatihan.java
+++ Latihan-Pertemuan1/src/VersionalControl/KelasLatihan.java
@@ -4,6 +4,10 @@ public class KelasLatihan {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		System.out.println("Hai");
+		
+		System.out.println("Haloooo");
 	}
 
 }
